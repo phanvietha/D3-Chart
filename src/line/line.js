d2bca7224c0f22557c63a6810f2566d20fefaa92
Ha Phan <ha.pvha.phan@gmail.com>
2019-04-01 21:16:16 +0700

    Implements Line Chart

--- src/line/line.js
+++ src/line/line.js
@@ -3,6 +3,7 @@ import * as d3 from 'd3';
 import jsonData from '../data/coins.json';
 import 'webpack-jquery-ui/slider';
 import 'webpack-jquery-ui/css';
+import { contourDensity } from 'd3';
 
 
 const margin = { left: 80, right: 100, top: 50, bottom: 100 };
@@ -12,11 +13,9 @@ const width = 800 - margin.left - margin.right;
 const group = d3.select("#line-chart").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", `translate(${margin.left}, ${margin.top})`);
+    .append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-var parseTime = d3.timeParse("%d/%m/%Y");
-var formatTime = d3.timeFormat("%d/%m/%Y");
 
 const xScale = d3.scaleTime().range([0, width]);
 const yScale = d3.scaleLinear().range([height, 0]);
@@ -30,47 +29,92 @@ const yAxisCall = d3.axisLeft()
 const yAxis = group.append('g').attr('class', 'y axis');
 const xAxis = group.append('g').attr('class', 'x axis').attr('transform', `translate(0, ${height})`);
 
+// Parse a string to Day Object
+const parseTime = d3.timeParse('%d/%m/%Y');
+// Parse Date object to String with format 
+const formatTime = d3.timeFormat('%d/%m/%Y');
+
+const endDate = parseTime("31/10/2017").getTime();
+const startDate = parseTime("12/5/2013").getTime();
 
 
+let coins;
+
+const duration = () => d3.transition().duration(1000);
+
+// Create a range slider
 $("#date-slider").slider({
-    range: true,
-    max: parseTime("31/10/2017").getTime(),
-    min: parseTime("12/5/2013").getTime(),
-    step: 86400000, // One day
-    values: [parseTime("12/5/2013").getTime(), parseTime("31/10/2017").getTime()],
-    // slide: function(event, ui){
-    //     $("#dateLabel1").text(formatTime(new Date(ui.values[0])));
-    //     $("#dateLabel2").text(formatTime(new Date(ui.values[1])));
-    //     update();
-    // }
+    min: startDate,
+    max: endDate,
+    step: 86400000, // One Step mean one Day
+    values: [startDate, endDate], // Default value for slider
+    range: true, // Not allow range to crossing each other
+    slide: (_, ui) => {
+        const startDateMs = ui.values[0];
+        const endDateMs = ui.values[1];
+        // Change Date of text according to Slide position
+        $('#startDate').text(formatTime(new Date(startDateMs)));
+        $('#endDate').text(formatTime(new Date(endDateMs)));
+        // Update Line chart
+        update();
+    }
 });
 
+/**
+ * Transform raw data to clean data
+ * @param data coin object
+ */
+const cleanData = (data) => {
+    const filteredData = { ...data };
+    for (const coin in data) {
+        // Filter null price_usd
+        filteredData[coin] = data[coin].filter(obj => !(obj["price_usd"] == null));
+        filteredData[coin] = filteredData[coin].map(obj => ({
+            ...obj,
+            price_usd: +obj["price_usd"],
+            date: parseTime(obj["date"])
+        }));
+    }
+    return filteredData;
+}
 
-// const line = d3.line()
-//         .x(d => xScale(d.year))
-//         .y(d => yScale(d.))
 
-// d3.json("data/example.json").then(function(data) {
-//     // Data cleaning
-//     data.forEach(function(d) {
-//         d.year = parseTime(d.year);
-//         d.value = +d.value;
-//     });
 
-//     // Set scale domains
-//     x.domain(d3.extent(data, function(d) { return d.year; }));
-//     y.domain([d3.min(data, function(d) { return d.value; }) / 1.005, 
-//         d3.max(data, function(d) { return d.value; }) * 1.005]);
+const path = group.append('path')
+    .attr('class', 'path')
+    .attr('fill', 'none')
+    .attr('stroke', 'gray')
+    .attr('stroke-width', 1)
 
-//     // Generate axes once scales have been set
-//     xAxis.call(xAxisCall.scale(x))
-//     yAxis.call(yAxisCall.scale(y))
 
-//     // Add line to chart
-//     g.append("path")
-//         .attr("class", "line")
-//         .attr("fill", "none")
-//         .attr("stroke", "grey")
-//         .attr("stroke-with", "3px")
-//         .attr("d", line(data));
-// });
+const update = () => {
+    const sliderValues = $('#date-slider').slider('values');
+
+    const filterCoins = coins.filter(d => ((d.date >= sliderValues[0]) && (d.date <= sliderValues[1])));
+
+    // Update Scale
+    xScale.domain(d3.extent(filterCoins, (d) => d.date));
+    yScale.domain([d3.min(filterCoins, d => d['price_usd']), d3.max(filterCoins, d => d['price_usd'])]);
+    // Update axis with new Scale
+    xAxis.transition(duration()).call(xAxisCall.scale(xScale));
+    yAxis.transition(duration()).call(yAxisCall.scale(yScale));
+
+    const line = d3.line()
+        .x(d => xScale(d.date))
+        .y(d => yScale(d['price_usd']));
+
+
+    path.transition(duration()).attr('d', line(filterCoins));
+
+
+
+
+}
+
+
+const main = () => {
+    coins = cleanData(jsonData)['bitcoin'];
+    update();
+}
+
+main();
